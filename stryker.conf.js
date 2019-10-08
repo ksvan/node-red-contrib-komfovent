6faa08c204242bc0c4307a607273ab76dc4ebdae
ksvan <kristians@live.com>
2019-10-08 20:09:11 +0200

    upd config kclass

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,11 +1,17 @@
 module.exports = function(config) {
   config.set({
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["html", "clear-text", "progress"],
-    testRunner: "mocha tests/test_komfoclass_spec.js",
+    mutate: ['komfnodes/komfovent.js'],
+    mutator: 'javascript',
+    packageManager: 'npm',
+    reporters: ['html', 'clear-text', 'progress'],
+    testRunner: 'mocha',
+    mochaOptions: {
+      spec: ['./test/test_komfoclass_spec.js']
+      // require: ['./test/unit/testHelper.js']
+    },
     transpilers: [],
-    testFramework: "mocha",
-    coverageAnalysis: "perTest"
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    maxConcurrentRunners: 2
   });
 };
