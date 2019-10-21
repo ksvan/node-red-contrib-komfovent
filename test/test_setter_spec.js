894614e16bc4499b345ff7d3fd3dc4862481445b
ksvan <kristians@live.com>
2019-10-21 12:43:11 +0200

    refactor updates

--- test/test_setter_spec.js
+++ test/test_setter_spec.js
@@ -1,12 +1,14 @@
 /* eslint-env mocha */
 'use strict';
-let should = require('should');
-let nock = require('nock');
-let helper = require('node-red-node-test-helper');
-let komfoSetNode = require('../komfnodes/setter.js');
-let komfoConfNode = require('../komfnodes/config.js');
+const should = require('should');
+const nock = require('nock');
+const helper = require('node-red-node-test-helper');
+const komfoSetNode = require('../komfnodes/setter.js');
+const komfoConfNode = require('../komfnodes/config.js');
 
 const netScope = 'http://192.168.1.1';
+const ip = '192.168.1.1';
+const ip2 = '192.168.2.1';
 
 helper.init(require.resolve('node-red'));
 nock.disableNetConnect();
@@ -49,70 +51,73 @@ describe('Komfovent setter node-red', function () {
   });
 
   // the way node-red defines flows and relations between nodes, and sets their initial values
-  let flow = [
-    { id: 'nc', type: 'komfoventConfig', 'ip': '192.168.1.1', displayName: 'Komfovent Site', 'z': 'f1' },
-    { id: 'n1', type: 'komfoventNode', displayName: 'Komfo set Data', user: 'nc', wires: [['nh']], 'z': 'f1' },
-    { id: 'nh', type: 'helper', 'z': 'f1' },
+  const flow = [
+    { id: 'nc', type: 'komfoventConfig', ip: ip, displayName: 'Komfovent Site', z: 'f1' },
+    { id: 'n1', type: 'komfoventNode', displayName: 'Komfo set Data', user: 'nc', wires: [['nh']], z: 'f1' },
+    { id: 'nh', type: 'helper', z: 'f1' },
     { id: 'f1', type: 'tab', label: 'Test flow' }
   ];
   // seperat secret credentials object to be passed in at launch, adhering to how nodered protects secrets
-  let credentials = { nc: { 'username': 'user', 'password': '1234' } };
+  const credentials = { nc: { username: 'user', password: '1234' } };
 
+  describe('Node load with config', function () {
   // node should be loaded fine in the runtime
-  it('should be loaded', function (done) {
-    helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
-      let n1 = helper.getNode('n1');
-      n1.should.have.property('displayName', 'Komfo set Data');
-      done();
+    it('should be loaded', function (done) {
+      helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
+        const n1 = helper.getNode('n1');
+        n1.should.have.property('displayName', 'Komfo set Data');
+        done();
+      });
     });
+
+    // Node should have logon credentials needed
+    it('should have credentials', function (done) {
+      helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
+        const n1 = helper.getNode('n1');
+        n1.komfoUser.credentials.should.have.property('username', 'user');
+        n1.komfoUser.should.have.property('ip', ip);
+        n1.komfoUser.credentials.should.have.property('password', '1234');
+        // check if credentials are there
+        done();
+      });
+    }); // it end
   });
+  // Node should reply with error, unknown value
 
-  // Node should have logon credentials needed
-  it('should have credentials', function (done) {
-    helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
-      let n1 = helper.getNode('n1');
-      n1.komfoUser.credentials.should.have.property('username', 'user');
-      n1.komfoUser.should.have.property('ip', '192.168.1.1');
-      n1.komfoUser.credentials.should.have.property('password', '1234');
-      // check if credentials are there
-      done();
-    });
-  }); // it end
+  describe('Node setting mode', function () {
+    it('should return error', function (done) {
+      this.timeout(5000);
+      helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
+        const n1 = helper.getNode('n1');
+        const nh = helper.getNode('nh');
+        nh.on('input', msg => {
+          msg.payload.should.have.property('error', true);
+          msg.payload.should.have.property('result', 'Unsupported mode');
+          done();
+        });
+        n1.on('call:error', call => {
+          console.log('error: ' + call);
+        });
+        n1.receive({ payload: 'v_s1ert' }); // not existing mode
+      });
+    }); // it end
 
-// Node should reply with error, unknown value
-it('should return error', function (done) {
-    this.timeout(5000);
-    helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
-      let n1 = helper.getNode('n1');
-      let nh = helper.getNode('nh');
-      nh.on('input', msg =>  {
-        msg.payload.should.have.property('error', true);
-        msg.payload.should.have.property('result', 'Unsupported mode');
-        done();
+    // Node should reply with changed mode
+    it('should return changed mode w delay', function (done) {
+      this.timeout(5000);
+      helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
+        const n1 = helper.getNode('n1');
+        const nh = helper.getNode('nh');
+        nh.on('input', msg => {
+          msg.payload.should.have.property('error', false);
+          msg.payload.should.have.property('result', 'auto');
+          done();
+        });
+        n1.on('call:error', call => {
+          console.log('error: ' + call);
+        });
+        n1.receive({ payload: 'auto' }); //  existing mode
       });
-      n1.on('call:error', call => {
-        console.log("error: " + call)
-      });
-      n1.receive({ payload: 'v_s1ert' }); // not existing mode
-    });
-  }); // it end
-
-  // Node should reply with changed mode
-it('should return changed mode w delay', function (done) {
-    this.timeout(5000);
-    helper.load([komfoSetNode, komfoConfNode], flow, credentials, function () {
-      let n1 = helper.getNode('n1');
-      let nh = helper.getNode('nh');
-      nh.on('input', msg =>  {
-        msg.payload.should.have.property('error', false);
-        msg.payload.should.have.property('result', 'auto');
-        done();
-      });
-      n1.on('call:error', call => {
-        console.log("error: " + call)
-      });
-      n1.receive({ payload: 'auto' }); //  existing mode
-    });
-  }); // it end
-
+    }); // it end
+  });
 });
